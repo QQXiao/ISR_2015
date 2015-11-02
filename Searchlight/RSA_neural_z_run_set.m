9f698f2af7fd0210e61718ebd97f21b5ded7fcb3
QQxiao <xiaoxq@mail.bnu.edu.cn>
2015-11-02 08:45:25 +0800

    no idea

---
+++ Searchlight/RSA_neural_z_run_set.m
@@ -0,0 +1,164 @@
+function RSA_neural_z_run(subs)
+Mtrial=1; % trial number
+MpID=2;  % material id_pic
+MwID=3;  % material id_word
+Mcat1=4; % 1=structure,2=nature
+Mcat2=5; % 1=structure-foreign,2=structure-local,3=nature-water,4=nature-land
+
+Mres=6; %% 1=structure-foreign,2=structure-local,3=nature-water,4=nature-land
+MRT=7; % reaction time;
+
+Monset=8; % designed onset time
+MAonset=9; % actually onset time
+Mrun=10;
+Mset=11;
+MAonset_r=12; % actually onset time for response 1;category;
+Mscore=13;%right or wrong for identity
+%%added information
+Mposit=14;
+Mmem=15;
+Msub=16;
+Mphase=17;
+%%%%%%%%%
+xlength =  112;
+ylength =  112;
+zlength =  64;
+radius  =  2;     % the cubic size is (2*radius+1) by (2*radius+1) by (2*radius+1)
+step    =  1;     % compute accuracy map for every STEP voxels in each dimension
+epsilon =  1e-6;
+TN=96*2
+%%%%%%%%%
+basedir='/seastor/helenhelen/ISR_2015';
+labeldir=[basedir,'/behav/label'];
+datadir=sprintf('%s/data_singletrial/ref_space/zscore/beta/merged',basedir);
+rdir=sprintf('%s/Searchlight_RSM/ref_space/zscore_sep_run/r',basedir);
+addpath /seastor/helenhelen/scripts/NIFTI
+%%%%%%%%%
+for sub=subs
+ERS_r=zeros(xlength,ylength,zlength,6);
+mem_r=zeros(xlength,ylength,zlength,3);
+ln_r=zeros(xlength,ylength,zlength,3);
+        %perpare data
+        load(sprintf('%s/encoding_sub%02d.mat',labeldir,sub));
+        list_subln=subln;
+        list_subln(:,Msub)=sub;
+        list_subln(:,Mphase)=1;
+        load(sprintf('%s/test_sub%02d.mat',labeldir,sub));
+        list_submem=submem;
+        list_submem(:,Msub)=sub;
+        list_submem(:,Mphase)=2;
+        for nn=1:96
+        p=list_subln(nn,MpID);w=list_subln(nn,MwID);
+        list_subln(nn,Mmem)=list_submem(list_submem(:,MpID)==p & list_submem(:,MwID)==w,Mmem);
+        end
+        all_label=[list_subln;list_submem];
+
+        all_idx=1:TN*(TN-1)/2; %% all paired correlation idx;
+        all_pID1=[]; all_pID2=[]; all_wID1=[]; all_wID2=[]; all_Rcate=[]; all_mem1=[]; all_mem2=[]; all_phase1=[]; all_phase2=[]; all_cate1=[];  all_cate2=[]; check_run=[]; check_set=[]; check_cate=[];all_run1=[];all_run2=[];all_set1=[];all_set2=[];
+        for k=2:TN
+        all_pID1=[all_pID1 all_label(k-1,MpID)*ones(1,TN-k+1)];
+        all_pID2=[all_pID2 all_label(k:TN,MpID)'];
+        all_wID1=[all_wID1 all_label(k-1,MwID)*ones(1,TN-k+1)];
+        all_wID2=[all_wID2 all_label(k:TN,MwID)'];
+        all_mem1=[all_mem1 all_label(k-1,Mmem)*ones(1,TN-k+1)];
+        all_mem2=[all_mem2 all_label(k:TN,Mmem)'];
+        all_phase1=[all_phase1 all_label(k-1,Mphase)*ones(1,TN-k+1)];
+        all_phase2=[all_phase2 all_label(k:TN,Mphase)'];
+        all_cate1=[all_cate1 all_label(k-1,Mcat2)*ones(1,TN-k+1)];
+        all_cate2=[all_cate2 all_label(k:TN,Mcat2)'];
+        all_run1=[all_run1 all_label(k-1,Mrun)*ones(1,TN-k+1)];
+        all_run2=[all_run2 all_label(k:TN,Mrun)'];
+        all_set1=[all_set1 all_label(k-1,Mset)*ones(1,TN-k+1)];
+        all_set2=[all_set2 all_label(k:TN,Mset)'];
+
+	%1=same run;0=diff run
+        check_run=[check_run (all_label(k:TN,Mrun)==all_label(k-1,Mrun))'];
+
+        %1=same set;0=diff set
+        check_set=[check_set (all_label(k:TN,Mset)==all_label(k-1,Mset))'];
+        %1=same category;0=diff categories
+        check_cate=[check_cate (all_label(k:TN,Mcat2)==all_label(k-1,Mcat2))'];
+        end
+        %% get indexes
+        % ERS
+	for r=1:2
+	for s=1:2
+        idx_ERS_I{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==2 & all_mem2==1 & all_pID1==all_pID2 & all_wID1==all_wID2);%identity pair: p+c+
+	idx_ERS_IB_all{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==2 & all_mem2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==1);
+	idx_ERS_IB_wc{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==2 & all_mem2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==1 & check_cate==1);
+        idx_ERS_D{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==2 & all_mem2==1 & all_pID1==all_pID2 & all_wID1~=all_wID2);%%same face different words: p+c-
+        idx_ERS_DB_all{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==2 & all_mem2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==0);
+        idx_ERS_DB_wc{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==2 & all_mem2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==0 & check_cate==1);
+        %mem
+        idx_mem_D{r,s}=find(all_run1==r & all_set1==s & all_phase1==2 & all_phase2==2 & all_mem1 ==1 & all_mem1==1 & all_pID1==all_pID2 & all_wID1~=all_wID2);%%same face different words: p+c-
+        idx_mem_DB_all{r,s}=find(all_run1==r & all_set1==s & all_phase1==2 & all_phase2==2 & all_mem1==1 & all_mem2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==0);
+        idx_mem_DB_wc{r,s}=find(all_run1==r & all_set1==s & all_phase1==2 & all_phase2==2 & all_mem1==1 & all_mem2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==0 & check_cate==1);
+        %ln
+        idx_ln_D{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==1 & all_pID1==all_pID2 & all_wID1~=all_wID2);%%same face different words: p+c-
+	idx_ln_DB_all{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==0);
+	idx_ln_DB_wc{r,s}=find(all_run1==r & all_set1==s & all_phase1==1 & all_phase2==1 & all_pID1~=all_pID2 & check_run==1 & check_set==0 & check_cate==1);
+
+	%get fMRI data
+        data_file=sprintf('%s/sub%02d.nii.gz',datadir,sub);
+        data_all=load_nii_zip(data_file);
+        data=data_all.img;
+
+        %%analysis
+        for k=radius+1:step:xlength-radius
+            for j=radius+1:step:ylength-radius
+                for i=radius+1:step:zlength-radius
+                    data_ball = data(k-radius:k+radius,j-radius:j+radius,i-radius:i+radius,:); % define small cubic for memory data
+                    a=size(data_ball);
+                    b=a(1)*a(2)*a(3);
+                    v_data = reshape(data_ball,b,TN);
+                    %bb=reshape(v_data,b*TN,1);
+                    if sum(std(v_data(:,:))==0)>epsilon
+                    ERS_r(k,j,i,:)=10;
+                    mem_r(k,j,i,:)=10;
+                    ln_r(k,j,i,:)=10;
+                    else
+                    xx=v_data';
+                    cc=1-pdist(xx(:,:),'correlation');
+                    ERS_r(k,j,i,1)=mean(cc(idx_ERS_I{r,s}));
+                    ERS_r(k,j,i,2)=mean(cc(idx_ERS_IB_wc{r,s}));
+                    ERS_r(k,j,i,3)=mean(cc(idx_ERS_IB_all{r,s}));
+                    ERS_r(k,j,i,4)=mean(cc(idx_ERS_D{r,s}));
+                    ERS_r(k,j,i,5)=mean(cc(idx_ERS_DB_wc{r,s}));
+                    ERS_r(k,j,i,6)=mean(cc(idx_ERS_DB_all{r,s}));
+
+                    mem_r(k,j,i,1)=mean(cc(idx_mem_D{r,s}));
+                    mem_r(k,j,i,2)=mean(cc(idx_mem_DB_wc{r,s}));
+                    mem_r(k,j,i,3)=mean(cc(idx_mem_DB_all{r,s}));
+
+                    ln_r(k,j,i,1)=mean(cc(idx_ln_D{r,s}));
+                    ln_r(k,j,i,2)=mean(cc(idx_ln_DB_wc{r,s}));
+                    ln_r(k,j,i,3)=mean(cc(idx_ln_DB_all{r,s}));
+                    end %end if
+                end %end i
+            end %end j
+        end %end k
+
+
+
+cd (rdir)
+       filename=sprintf('ERS_sub%02d_run%s_set%s.nii',sub,r,s);
+       data_all.img=squeeze(ERS_r(:,:,:,:));
+       data_all.hdr.dime.dim(5)=6; % dimension chagne to 6
+       save_untouch_nii(data_all, filename);
+       system(sprintf('gzip -f %s',filename));
+
+        filename=sprintf('mem_sub%02d_run%s_set%s.nii',sub,r,s);
+        data_all.img=squeeze(mem_r(:,:,:,:));
+        data_all.hdr.dime.dim(5)=3; % dimension chagne to 3
+        save_untouch_nii(data_all, filename);
+        system(sprintf('gzip -f %s',filename));
+
+        filename=sprintf('ln_sub%02d_run%s_set%s.nii',sub,r,s);
+        data_all.img=squeeze(ln_r(:,:,:,:));
+        data_all.hdr.dime.dim(5)=3; % dimension chagne to 3
+        save_untouch_nii(data_all, filename);
+        system(sprintf('gzip -f %s',filename));
+end %set
+end %run
+end %end sub
+end %end func
