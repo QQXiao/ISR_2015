9b110066ad70fb44a126f206253932b5cd448718
QQXiao <xiaoxq@mail.bnu.edu.cn>
2015-11-23 21:30:30 +0800

    modified:   merge_all_sub_data.m

--- ROI_based/merge_all_sub_data.m
+++ ROI_based/merge_all_sub_data.m
@@ -5,15 +5,17 @@ resultdir=sprintf('%s/me/data/item',basedir);
 subs=setdiff(1:21,2);
 ln=[];mem=[]
 for s=subs
-	tln=[];tmem=[]
+tln=[];tmem=[];
+	rln=[];rmem=[]
 	load(sprintf('%s/mem_sub%02d.mat',datadir,s));
 	load(sprintf('%s/ln_sub%02d.mat',datadir,s));
 	for r=1:13
-	tln=[tln squeeze(roi_ln(:,r,:))];
-	tmem=[tmem squeeze(roi_mem(:,r,:))];
+	rln=[rln squeeze(roi_ln(:,r,:))];
+	rmem=[rmem squeeze(roi_mem(:,r,:))];
 	end
-	ln=[s*ones(48,1) [1:48]' tln];
-	mem=[s*ones(48,1) [1:48]' tmem];
+	tln=[s*ones(48,1) [1:48]' rln];
+	tmem=[s*ones(48,1) [1:48]' rmem];
+ln=[ln;tln]; mem=[mem;tmem];
 end
 eval(sprintf('save %s/mem.txt mem -ascii -tabs', resultdir));
 eval(sprintf('save %s/ln.txt ln -ascii -tabs', resultdir));
