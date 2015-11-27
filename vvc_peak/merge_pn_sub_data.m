855b576c02b3af6a5289532118738822f7782452
QQXiao <xiaoxq@mail.bnu.edu.cn>
2015-11-27 10:04:54 +0800

    modified:   merge_pn_sub_data.m

--- vvc_peak/merge_pn_sub_data.m
+++ vvc_peak/merge_pn_sub_data.m
@@ -12,9 +12,10 @@ ln=[];mem=[];
 	lnfile=sprintf('%s/ln_ln_sub%02d.mat',pndir,s);           
 	memfile=sprintf('%s/mem_mem_sub%02d.mat',pndir,s);           
 	load(lnfile);load(memfile);
-	sizel=size(ln_ln);
-	ln=[s*ones(sizel(1)) [1:sizel(1)]' ln_ln(:,[2 3])];
-	mem=[s*ones(sizel(1)) [1:sizel(1)]' mem_mem(:,[2 3])];
+	n=60:5:95
+	sizel=length(n);
+	ln=[s*ones(sizel(1),1) n' ln_ln(n,[2 3])];
+	mem=[s*ones(sizel(1),1) n' mem_mem(n,[2 3])];
 	ln_all=[ln_all;ln];mem_all=[mem_all;mem];
 end %sub
 eval(sprintf('save %s/allsub_ln.txt ln_all -ascii -tabs', pndir));
