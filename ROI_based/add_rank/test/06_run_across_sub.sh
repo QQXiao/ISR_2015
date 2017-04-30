40751a4ac6bc6a31cf762c818c075ea427509a42
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2017-04-30 16:30:49 -0700

    save representatonal space for each permutation

--- ROI_based/add_rank/test/06_run_across_sub.sh
+++ ROI_based/add_rank/test/06_run_across_sub.sh
@@ -1,8 +1,8 @@
 #!sh/bin/
 for ((r=1; r<=19; r++))
 do
-     fsl_sub -m abe -M water.read@gmail.com -q veryshort.q matlab -nodesktop -nosplash -r "get_representation_space($r);quit;"
-     fsl_sub -m abe -M water.read@gmail.com -q verylong.q matlab -nodesktop -nosplash -r "get_representation_space_baseline($r);quit;"
+     fsl_sub -m abe -M water.read@gmail.com -q veryshort.q matlab -nodesktop -nosplash -r "get_representation_space_save($r);quit;"
+     fsl_sub -m abe -M water.read@gmail.com -q verylong.q matlab -nodesktop -nosplash -r "get_representation_space_baseline_save($r);quit;"
  done
 #for ((ms=1; ms<=21; ms++))
 #do
