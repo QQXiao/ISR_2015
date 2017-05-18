029152c04d2937f03b477dbbe7661fba53809451
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2017-05-18 08:04:21 +0800

    modified sh for run

--- ROI_based/add_rank/test4/01_02_run_Cal_RepresentationSpace_Baseline.sh
+++ ROI_based/add_rank/test4/01_02_run_Cal_RepresentationSpace_Baseline.sh
@@ -1,8 +1,14 @@
 #!sh/bin/
-for ((r=1; r<=19; r++))
+rstart=$1
+rend=$2
+for ((r=rstart; r<=rend; r++))
+#for ((r=1; r<=19; r++))
 do
-    for subs in 1 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
+substart=$3
+subend=$4
+    for ((subs=substart; subs<=subend; subs++))
+    #for subs in 1 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
     do
-        fsl_sub -m abe -M water.read@gmail.com -q verylong.q matlab -nodesktop -nosplash -r "Cal_RepresentationSpace_Baseline($r,$subs);quit;"
+        fsl_sub -m abe -M water.read@gmail.com -q short.q matlab -nodesktop -nosplash -r "Cal_RepresentationSpace_Baseline($r,$subs);quit;"
     done
- done
+done
