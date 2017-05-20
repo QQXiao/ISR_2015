345f30f5e03cb4fdda56b237c1abcf54855d1acc
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2017-05-21 06:00:53 +0800

    modified cal equi script

--- ROI_based/add_rank/test4/04_01_Cal_NumOFEqui_RepresentationSpace.sh
+++ ROI_based/add_rank/test4/04_01_Cal_NumOFEqui_RepresentationSpace.sh
@@ -4,6 +4,6 @@ do
     for subs in 1 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21
     do
         fsl_sub -m abe -M water.read@gmail.com -q verylong.q matlab -nodesktop -nosplash -r "Cal_EquivalentInRepresentationSpace($r,$subs);quit;"
+    sleep 120
     done
-    sleep 200
  done
