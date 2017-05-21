d0d5e0ebc1777c57d36317d0881f48edc4798915
XiaoqianXiao <xiao.xiaoqian.320@gmail.com>
2017-05-22 06:41:34 +0800

    modified sh scripts

--- ROI_based/add_rank/test4/02_01_run_Cal_Similarity_RepresentationSpace.sh
+++ ROI_based/add_rank/test4/02_01_run_Cal_Similarity_RepresentationSpace.sh
@@ -2,4 +2,5 @@
 for ((r=1; r<=19; r++))
 do
     fsl_sub -m abe -M water.read@gmail.com -q veryshort.q matlab -nodesktop -nosplash -r "Cal_Similarity_RepresentationSpace($r);quit;"
- done
+    sleep 100
+done
